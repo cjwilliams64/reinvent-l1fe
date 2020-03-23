78e68a20a2022b40a0d3fb427f73ad0eb7ba2cb4
cjwilliams64 <w.chelsea64@gmail.com>
2020-03-22 22:48:51 -0400

    Add files via upload
    This is a result of a short Udemy tutorial from Bluelime Learning Solutions. I created an embedded web browser (internet explorer). It's a bit buggy, as it uses an outdated version of Internet Explorer, but it is fully functional. The logic used is in the Form1.cs file.
    
    I plan to see how other tools can be used to create a web browser.

---
+++ WebBrowser/RedLine/RedLine/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RedLine
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void cboLocation_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Enter)
+            {
+                //when the user presses the Enter Key, the embedded web browser will navigate to the result of the query entered (the text)
+                webBrowser1.Navigate(cboLocation.Text);
+              
+
+            }
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoBack();
+        }
+
+        private void btnForward_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoForward();
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate(cboLocation.Text);
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            Text = "redline - " + webBrowser1.Document.Title;
+
+            //title of the doc that was just loaded
+
+            // ex: browser will say 'redline - ABC Home Page - ABC.com' when you ask the browser to go to www.abc.com
+        }
+    }
+}
